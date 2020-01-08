7594c17f9068800ed57083eb525300166c2b1836
Tedu <1374050949@qq.com>
2020-01-08 10:04:22 +0800

    add code03

---
+++ code03.py
@@ -0,0 +1,17 @@
+"""
+    Variable
+"""
+
+# get name
+a = "Hello"
+b = "World"
+
+class_name = "1903"
+stu_name = "zs"
+result = class_name + stu_name
+print(result)
+
+name = input("please input name:")
+age = input("please input age:")
+print("the name is :"+name)
+print("the age is :"+age)
